546ed7fb0fa8bdc6633c89ae076de161b3ce7b39
j8 <j8@j6.io>
2015-11-24 16:16:07 +0200

    list book by id

--- server.js
+++ server.js
@@ -8,11 +8,9 @@ var client = new elasticsearch.Client({
 var server = restify.createServer();
 server.use(restify.queryParser());
 
-// Elastic search dump
+// Fetch all books
 
-server.get('/hello/:name', function(req, res, next) {
-
-console.log("Dump page", req.query.page);
+server.get('/books', function(req, res, next) {
 
   var perPage = 10
   , page = Math.max(0, req.query.page) - 1;
@@ -55,6 +53,54 @@ console.log("Dump page", req.query.page);
   });
 
 });
+
+
+// Fetch book by id
+
+server.get('/books/:id', function(req, res, next) {
+
+  var perPage = 10
+  , page = Math.max(0, req.query.page) - 1;
+
+  // Job.search({
+  //   match_all : {}
+  // },{
+  //     from: perPage * page,
+  //     size: perPage,
+  //     hydrate:true
+  // }, function(err, results) {
+  //     if (err) {
+  //         return res.status(500).json({
+  //             error: 'Cannot update the job ' + err 
+  //         });
+  //     }
+  //     // results here
+  //     res.json(results.hits)
+  // });
+
+  client.search({
+    index: 'books',
+    type: 'book',
+    body: {
+	query: {
+	match: {
+	  id: req.params.id
+	}
+		// match_all : {}
+	},
+	from: perPage * page,
+	size: perPage,
+    }
+  }).then(function (resp) {
+      var hits = resp.hits.hits;
+	  res.send(hits);
+	  next();
+  }, function (err) {
+      console.trace(err.message);
+  });
+
+});
+
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
